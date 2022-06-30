05e3bca41eb36740a5800ff0e95824729e101bc9
emilianospinoso <emilianospinoso@gmail.com>
2022-06-30 20:20:55 -0300

    include footer y header

--- app.py
+++ app.py
@@ -33,6 +33,10 @@ def index():
 def destroy(id):
     conn=mysql.connect()
     cursor=conn.cursor()
+
+    cursor.execute("SELECT foto from empleados WHERE id=%s", id)
+    fila = cursor.fetchall()  
+    os.remove(os.path.join(app.config['CARPETA'], fila[0][0]))
     cursor.execute("DELETE FROM empleados WHERE id=%s", (id))
     conn.commit()
     return redirect('/')
